e0f49f716ae7086597e720b307c7f2057282a6f0
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-11-13 22:12:14 +0300

    init

---
+++ src/test/java/com/jpa/hibernateApp/HibernateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.hibernateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
